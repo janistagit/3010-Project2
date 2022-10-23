860a31d65f42ee424c171cbbe6acd7700ef2e565
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2022-10-22 23:26:53 -0700

    Create class file

---
+++ Project2.java
@@ -0,0 +1,14 @@
+/*
+ * Project 2
+ * By Janista Gitbumrungsin
+ * CS3010.02 Fall 2022
+ * Dr. Lajpat Rai Raheja
+ */
+
+class Project2
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
